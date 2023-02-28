21cb83df7c013dcd1b6718340100a7bccc650600
Kim Martini <37541937+graphicfox@users.noreply.github.com>
2023-02-28 11:46:45 +0000

    feat: add php71 for old projects

---
+++ php71/boilerplate/opt/development.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# (BOOT-dev) Run scripts for your development environment.
+# Executed when the development container boots up. Can be used to simulate additional build steps
+# you would normally put into "build.sh" but don't need for your production container
+
+# Check if we have additional dev dependencies to install
+if [ -f "/opt/project/install.done" ]; then
+  echo "Dev dependencies have already been installed (marker exists at: /opt/project/install.done)"
+else
+  echo "Installing dev-only dependencies"
+
+  # Install dev-symlinker
+  composer global require labor-digital/composer-dev-symlink
+
+  # Set the marker
+  touch "/opt/project/install.done"
+fi
+
+# Install composer dependencies
+if [ -f "/var/www/html/composer.json" ]; then
+	export COMPOSER_PROCESS_TIMEOUT=1200
+
+	cd /var/www/html
+	composer install
+fi
