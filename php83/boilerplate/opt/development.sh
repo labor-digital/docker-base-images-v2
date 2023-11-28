aa6a7b509355af472de557c436c993aa88b9ae8e
Alex Steckler <info@steckler-alex.de>
2023-11-28 17:50:29 +0100

    fix: delete dev-symlinker in 82 and 83

--- php83/boilerplate/opt/development.sh
+++ php83/boilerplate/opt/development.sh
@@ -3,9 +3,6 @@
 # Executed when the development container boots up. Can be used to simulate additional build steps
 # you would normally put into "build.sh" but don't need for your production container
 
-# Install dev-symlinker
-composer global require labor-digital/composer-dev-symlink
-
 # Install composer dependencies
 if [ -f "/var/www/html/composer.json" ]; then
 	export COMPOSER_PROCESS_TIMEOUT=1200
